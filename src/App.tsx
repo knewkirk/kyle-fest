2f2eda70b4f6afea3dd3596068914f4f3cf73ad7
Kyle Newkirk <kylednewkirk@gmail.com>
2023-03-10 15:42:49 -0800

    Add yarn, ts, react, webpack, babel

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import './App.css';
+
+export default () => (
+  <div>
+  <h1>KYLE FEST</h1>
+  <h2>APRIL 8, 2023</h2>
+  </div>
+);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx', '.tsx'] },
+  output: {
+    path: path.resolve(__dirname, 'public/'),
+    publicPath: '/public/',
+    filename: 'bundle.js',
+  },
+  // devServer: {
+  //   contentBase: path.join(__dirname, 'public/'),
+  //   port: 3000,
+  //   publicPath: 'http://localhost:3000/dist/',
+  //   hotOnly: true,
+  // },
+  // plugins: [new webpack.HotModuleReplacementPlugin()],
+};
